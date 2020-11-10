e45525d4efda3b2fcc228691de27a8b744c16083
unknown <vrnakano@gmail.com>
2020-11-10 19:17:49 -0300

    prática Spring

--- webmvc/src/test/java/com/example/demo/WebmvcApplicationTests.java
+++ webmvc/src/test/java/com/example/demo/WebmvcApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test; 
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WebmvcApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testSomar() {
+		System.out.println("somar");
+		int n1 = 5;
+		int n2 = 5;
+		assertEquals
 	}
-
+	
+	@Test
+	public void testSomar2() {
+		System.out.println("somar");
+		int n1 = 5;
+		int n2 = 7;
+		assertEquals(n1, n2);
+	}
 }
